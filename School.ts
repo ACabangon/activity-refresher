7acad2f5d88691c446e743bbb39d57f09deddf0c
ACabangon <cabangon.angelyn@gmail.com>
2022-02-22 19:25:20 +0800

    done commit

--- School.ts
+++ School.ts
@@ -37,10 +37,18 @@ const studentList = new StudentList([
     student3
 ]);
 
-console.log(studentList.getStudents());
+//console.log(studentList.getStudents());
 // console.log("*************");
-// console.log(studentList.updateStudentName("2015-105869", "Michael Jordan"));
+//console.log(studentList.updateStudentName("2015-105869", "Michael Jordan"));
 // console.log("*************");
-console.log(studentList.updateAge("2015-105869", 25));
-// console.log(studentList.checkStudentId("2015-105870"));
-console.log(studentList.getStudents());
+//console.log(studentList.updateAge("2015-105869", 25));
+//console.log(studentList.checkStudentId("2015-105869"));
+///console.log(studentList.getStudents());
+
+// console.log(studentList.getStudent("2015-105869"));
+// console.log(studentList.updateCourse("2015-105869", "BSICT"));
+// console.log(studentList.updateCollege("2015-105869", "College of Business"));
+// console.log(studentList.updateAddress("2015-105869", "Mindanao"));
+console.log(studentList.getNumbers());
+console.log(studentList.removeSudent("2015-105868"));
+console.log(studentList.getNumbers());
--- StudentList.ts
+++ StudentList.ts
@@ -27,15 +27,16 @@ export class StudentList {
     }
 
     checkStudentId(id: string) {
-        // let valid = false;
+    //      let valid = false;
 
-        // for (let index = 0; index < this.students.length; index++) {
-        //     const element = this.students[index];
+    //  for (let index = 0; index < this.students.length; index++) {
+    //         const element = this.students[index];
 
-        //     if (element.studentId === id) {
-        //         valid = true;
-        //     }
-        // }
+    //          if (element.studentId === id) {
+    //              valid = true;
+    //          }
+    //     }
+
         let checkStudent = this.students.find(element => element.studentId === id);
 
         if (checkStudent) return true;
@@ -43,31 +44,61 @@ export class StudentList {
         return false;
     }
 
+
     updateAge(id: string, age: number) {
         let student = this.students.find(element => element.studentId === id);
-
+        
         if (student) {
             student.age = age;
             return student;
         }
+    }
 
-        return "Student id doesnt exists";
+    getStudent(id: string){
+        return this.students.find((student: Student) => student.studentId === id);
     }
 
     updateCourse(id: string, courseName: string) {
+        let student = this.students.find(element => element.studentId === id);
 
+        if (student){
+            student.course = courseName;
+            return student;
+        }
     }
 
     updateCollege(id: string, collegeName: string) {
+        let student = this.students.find(element => element.studentId === id);
 
+        if (student){
+            student.college = collegeName;
+            return student;
+        }
     }
 
     updateAddress(id: string, address: string) {
+        let student = this.students.find(element => element.studentId === id);
 
+        if (student){
+            student.address = address;
+            return student;
+        }
     }
 
-    //test
-
+    //try:
     // create method and return number of students
+    getNumbers(){
+        return this.students.length;
+    }
+    
     // delete one student and return list of students, use splice method (index, 1)
+    removeSudent(id: string){
+        for(var index = 0; index < this.students.length; index++){
+            if(this.students[index].studentId === id){
+                this.students.splice(index, 1);
+                break;
+            }
+        }
+        return "student remove, student id:" + id;
+    }
 }
